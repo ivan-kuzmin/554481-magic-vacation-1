eedaba6fbd2313ec2727d19f2f2b327b8ad08592
Ivan Kuzmin <vania_kuzmin@mail.ru>
2020-07-18 22:38:35 +0300

    added rules animation

--- source/js/modules/onload.js
+++ source/js/modules/onload.js
@@ -2,4 +2,10 @@ export default () => {
   document.body.onload = function () {
     document.body.classList.add(`loaded`);
   };
+
+  const lastRuleItem = document.getElementsByClassName('rules__item')[3];
+  const rulesLink = document.getElementsByClassName('rules__link')[0];
+  lastRuleItem.children[0].onanimationend = function (e) {
+    rulesLink.style.animationPlayState = 'running';
+  }
 };
