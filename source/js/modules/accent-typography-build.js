ec1c68b0497d11eafb2d3910c590f9a50a6dd1f3
Ivan Kuzmin <vania_kuzmin@mail.ru>
2020-08-11 14:54:30 +0300

    added accent-typography-build

---
+++ source/js/modules/accent-typography-build.js
@@ -0,0 +1,65 @@
+export default class AccentTypographyBuild {
+  constructor(params) {
+    this._duration = params.duration || 500;
+    this._delay = params.delay || 100;
+    this._wordDelay = params.wordDelay || 0;
+    this._element = params.element;
+
+    this.prePareText();
+  }
+
+  createElement(letter, letterIndex, wordIndex) {
+    const span = document.createElement(`span`);
+    span.textContent = letter;
+    span.style.margin = `0 -0.02em`;
+    span.style.display = `inline-block`;
+    span.style.opacity = 0;
+    span.style.transform = `translate3d(0, 100%, 0)`;
+    span.style.transitionProperty = `transform, opacity`;
+    span.style.transitionDuration = `${this._duration}ms`;
+    span.style.transitionTimingFunction = `ease`;
+    span.style.transitionDelay = `${(3 - letterIndex % 3) * this._delay + wordIndex * this._wordDelay}ms`;
+    return span;
+  }
+
+  prePareText() {
+    if (!this._element) {
+      return;
+    }
+    const text = this._element.textContent.trim().split(` `).filter((latter) => latter !== ``);
+
+    const content = text.reduce((fragmentParent, word, wordIndex) => {
+      const wordElement = Array.from(word).reduce((fragment, latter, letterIndex) => {
+        const letterContainer = this.createElement(latter, letterIndex, wordIndex);
+        fragment.appendChild(letterContainer);
+        return fragment;
+      }, document.createDocumentFragment());
+      const wordContainer = document.createElement(`span`);
+      wordContainer.style.display = `inline-flex`;
+      wordContainer.style.overflowY = `hidden`;
+      wordContainer.style.letterSpacing = 0;
+      wordContainer.style.padding = `0 0.02em`;
+      if (wordIndex !== text.length - 1) {
+        wordContainer.style.marginRight = `5px`;
+      }
+      wordContainer.appendChild(wordElement);
+      fragmentParent.appendChild(wordContainer);
+      return fragmentParent;
+    }, document.createDocumentFragment());
+
+    this._element.innerHTML = ``;
+    this._element.appendChild(content);
+  }
+
+  runAnimation() {
+    if (!this._element) {
+      return;
+    }
+    this._element.childNodes.forEach((word) => {
+      word.childNodes.forEach((letter) => {
+        letter.style.transform = `translate3d(0, 20%, 0)`;
+        letter.style.opacity = 1;
+      });
+    });
+  }
+}
--- source/js/script.js
+++ source/js/script.js
@@ -9,6 +9,7 @@ import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
 import onLoad from './modules/onload';
+import AccentTypographyBuild from './modules/accent-typography-build';
 
 // init modules
 mobileHeight();
@@ -23,3 +24,18 @@ onLoad();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+const introTitleAnimation = new AccentTypographyBuild({
+  element: document.querySelector(`.intro__title`),
+  wordDelay: 500
+});
+setTimeout(()=>{
+  introTitleAnimation.runAnimation();
+}, 500);
+
+const introDateAnimation = new AccentTypographyBuild({
+  element: document.querySelector(`.intro__date`)
+});
+setTimeout(()=>{
+  introDateAnimation.runAnimation();
+}, 1250);
